2566b1298dbbca352e8db962ead70645ccd56221
faked <liudas.v@yahoo.com>
2018-03-21 21:00:17 +0200

    ketvirtas uzdavinys

--- src/ketvirtasuzdavinys.java
+++ src/ketvirtasuzdavinys.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ketvirtasuzdavinys {
+
+    //4.      Parašyti programą, kuri paprašytų įvesti 5 skaičius.
+    // Baigus skaičių įvedimą, turi būti atvaizduojama įvestų skaičių suma ir visi įvesti skaičiai.
+
+    public static void main(String[] args) {
+        Scanner sk = new Scanner(System.in);
+        System.out.println("iveskite penkis skaicius");
+        int mas[]=new int[5];
+        for (int i = 0; i <5; i++){
+            System.out.println("iveskite " +i+" skaiciu");
+            mas[i]=sk.nextInt();
+        }
+        int sum=0;
+        for (int i =0;i<mas.length;i++){
+            sum+=mas[i];
+            System.out.println(mas[i]);
+        }
+
+        System.out.println(sum);
+
+
+
+    }
 }
---
+++ src/penktasuzdavinys.java
@@ -0,0 +1,9 @@
+public class penktasuzdavinys {
+    //*Parašyti programą kuri paprašytų vartotojo įvesti  5 žodžius.
+    // *Po to kai bus įvesti visi žodžiai, jie turi būti atvaizduojami.
+    public static void main(String[] args) {
+
+
+    }
+
+}
